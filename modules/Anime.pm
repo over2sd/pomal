7eb306e42d763a0ef6da423e328dc22d753e1c5f
Lincoln Sayger <skrey_pub@cox.net>
2014-09-04 11:10:15 -0500

    Series->modules 1/2

--- modules/Anime.pm
+++ modules/Anime.pm
@@ -25,7 +25,7 @@ print ".";
 package Series;
 
 sub new {
-	my ($class,$i,$n,@d) = @_;
+	my ($class,$i,$n,%d) = @_;
 	my $self = {
 		dbid => $i,
 		name => $n,
@@ -33,31 +33,115 @@ sub new {
 		watched_episode => 0,
 		started_date => "",
 		finished_date => "", # also valid for drop date
-		meta_data => {},
-		score => 0,
+		metadata => {},
+		title_score => 0,
 		obj_content => 0, # bitwise mask; described in Common.pm
-		min_age => 0
-		status => 0, # 0 - Plan-to-watch, 1 - Watching, 2 - On-hold, 3 - Rewatching, 4 - Completed, 5 - Dropped
+		min_age => 0,
+		tstat => 0, # 0 - Plan-to-watch, 1 - Watching, 2 - On-hold, 3 - Rewatching, 4 - Completed, 5 - Dropped
 		rewatched_episode => 0,
 		rewatched_times => 0
 	};
 	bless $self,$class;
+	if (%d) {
+		# TODO: process other details here
+#		foreach (keys %d) {
+# total_eps
+# watched_ep
+# score
+# status
+# content
+# rating
+# metaData
+#		}
+	}
 	return $self;
 }
 print ".";
 
 # id
+sub id {
+	my ($self,$value) = @_;
+	$self->{dbid} = $value if defined($value);
+	return $self->{dbid};
+}
+print ".";
+
 # title
-# total_eps
-# watched_ep
-# began
-# ended
-# metaData
-# score
-# status
-# rewatch_ended
-# content
-# rating
+sub title {
+	my ($self,$value) = @_;
+	$self->{name} = $value if defined($value);
+	return $self->{name};
+}
+print ".";
+
+sub total_eps {
+	my ($self,$value) = @_;
+	$self->{episodes} = $value if defined($value);
+	return $self->{episodes};
+}
+print ".";
+
+sub watched_ep {
+	my ($self,$value) = @_;
+	# check if value == max episodes, and pass back a second return value, because this may be called during import.
+	my $max = ($value >= $max) ? 1 : 0;
+	if ($self->{tstat} eq "RW") {
+		$self->{rewatched_episode} = $value if defined($value);
+		return $self->{rewatched_episode},$max,0;		
+	} else {
+		$self->{watched_episode} = $value if defined($value);
+		# check for not status Watching and pass back an indicator to the caller that it needs to be set to complete.
+		my $notwatch = ($self->{tstat} ne "WA") ? 1 : 0;
+		return $self->{watched_episode},$max,$notwatch;
+	}
+}
+print ".";
+
+sub began {} # TODO: write date handlers
+
+sub ended {} # TODO: write date handlers
+
+sub metaData {
+	my ($self,$key,$value) = @_;
+	unless (defined $key) { return undef; }
+	unless (defined $value) { return $self->{metadata}{$key}; }
+#	print "Setting metadata $key to $value.\n";
+	($self->{metadata}{$key} = $value) or return undef;
+	return $value;
+}
+print ".";
+
+sub score {
+	my ($self,$value) = @_;
+	$self->{title_score} = $value if defined($value);
+	return $self->{title_score};
+}
+print ".";
+
+sub status {
+	my ($self,$value) = @_;
+	$self->{tstat} = $value if defined($value);
+	return $self->{tstat};
+}
+print ".";
+
+sub rewatch_ended {} # TODO: write date handlers
+
+sub content {
+	# Takes a bitwise mask for $value! Do all processing of mask in calling function!
+	my ($self,$value) = @_;
+	$self->{obj_content} = $value if defined($value);
+	return $self->{obj_content};
+}
+print ".";
+
+sub rating {
+	my ($self,$value) = @_;
+	$self->{min_age} = $value if defined($value);
+	return $self->{min_age};
+}
+print ".";
+
 
 ##############################  General Functions ##############################
 package Anime;
---
+++ modules/Common.pm
@@ -0,0 +1,64 @@
+package Common;
+
+my @obj = qw( nudity violence language sex brutality blasphemy horror nihilism theology occult superpowers rape fanservice drugs hentai gambling war discrimination );
+my %objindex;
+@objindex{@obj} = (0..$#obj);
+# used by contentMask and by contentList
+sub contentMask {
+	my ($key,$mask) = @_;
+	# get position, or position just outside the array
+	# (won't bother getBit, but beware using this return value elsewhere!)
+	my $pos = get(\$objindex,$key,scalar @obj);
+	unless (defined $mask) { return $pos; }
+	return getBit($pos,$mask); # if passed a mask, return true/false that that key's bit is set in the mask.
+}
+print ".";
+
+# contentList can be used by option setter to choose which types of content to allow to be noted/flitered
+sub contentList { return @obj; }
+print ".";
+
+sub getBit { # returns bool
+	my ($pos,$mask) = @_;
+	$pos = 2**$pos;
+	return ($mask & $pos) == $pos ? 1 : 0;
+}
+print ".";
+
+sub setBit { # returns mask
+	my ($pos,$mask) = @_;
+	$pos = 2**$pos;
+	return $mask | $pos;
+}
+
+sub unsetBit { # returns mask
+	my ($pos,$mask) = @_;
+	$pos = 2**$pos;
+	return $mask ^ $pos;
+}
+print ".";
+
+sub toggleBit { # returns mask
+	my ($pos,$mask) = @_;
+	$pos = 2**$pos;
+	$pos = $mask & $pos ? $pos : $pos * -1;
+	return $mask + $pos;
+}
+print ".";
+
+sub get {
+	my ($hr,$key,$dv) = @_;
+	if ((not defined $hr) or (not defined $key) or (not defined $dv)) {
+		warn "Safe getter called without required parameter(s)! ($hr,$key,$dv)";
+		return undef;
+	}
+	if (exists $hr->{$key}) {
+		return $hr->{$key};
+	} else {
+		return $dv;
+	}
+}
+print ".";
+
+print __PACKAGE__ . " OK; ";
+1;
